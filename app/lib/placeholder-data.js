3c3eb51f9c74fba6c41f2cd5af78e14310c55a0d
WIN-LGH8LNL3429\EDY <1633584401@qq.com>
2024-03-14 15:33:26 +0800

    finish

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -7,6 +7,12 @@ const users = [
     email: 'user@nextmail.com',
     password: '123456',
   },
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    name: 'User',
+    email: '1633584401@qq.com',
+    password: '123456',
+  },
 ];
 
 const customers = [
